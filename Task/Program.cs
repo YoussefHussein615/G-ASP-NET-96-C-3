92bd8cb2359481ea4b7273e88c34e79bfa112842
EL3RABAWY <yshm2@sci.suezuni.edu.eg>
2026-07-20 17:07:31 +0300

    question2

--- Task/Program.cs
+++ Task/Program.cs
@@ -6,14 +6,22 @@
         {
             #region Question 1
 
-            string title = "Clean code";
-            title = title.ToUpper();
-            string upperTitle = title.ToUpper();
-            Console.WriteLine(title);
-            Console.WriteLine(upperTitle);
+            //string title = "Clean code";
+            //title = title.ToUpper();
+            //string upperTitle = title.ToUpper();
+            //Console.WriteLine(title);
+            //Console.WriteLine(upperTitle);
 
             #endregion
-        }
 
+            #region Question 2
+            string title = "Clean code";
+            string title2 = "Clean code";
+            bool areSame = ReferenceEquals(title, title2);
+            Console.WriteLine(areSame);
+
+            #endregion
+
+        }
     }
 }
