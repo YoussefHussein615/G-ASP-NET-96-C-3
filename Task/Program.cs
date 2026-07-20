5340f8f00777c3d33a81bddf554874e9cb4e5598
EL3RABAWY <yshm2@sci.suezuni.edu.eg>
2026-07-20 17:01:04 +0300

    Question1

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,11 +5,13 @@
         static void Main(string[] args)
         {
             #region Question 1
+
             string title = "Clean code";
             title = title.ToUpper();
             string upperTitle = title.ToUpper();
             Console.WriteLine(title);
             Console.WriteLine(upperTitle);
+
             #endregion
         }
 
