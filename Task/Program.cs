d9d2439b930c2ad35b93d8f90a4d0a9ee1e96da6
EL3RABAWY <yshm2@sci.suezuni.edu.eg>
2026-07-20 16:55:37 +0300

    Add project files.

---
+++ Task/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            string title = "Clean code";
+            title = title.ToUpper();
+            string upperTitle = title.ToUpper();
+            Console.WriteLine(title);
+            Console.WriteLine(upperTitle);
+            #endregion
+        }
+
+    }
+}
