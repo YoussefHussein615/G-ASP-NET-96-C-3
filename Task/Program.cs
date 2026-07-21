a752494fda40f5cae1fede52874c5b263f1d3096
EL3RABAWY <yshm2@sci.suezuni.edu.eg>
2026-07-21 15:16:21 +0300

    question 10

--- Task/Program.cs
+++ Task/Program.cs
@@ -95,6 +95,24 @@ namespace Task
             //}
             #endregion
 
+            #region Question10
+            //{
+            //    string title = "Refactoring";
+            //    switch (title)
+            //    {
+            //        case "Clean Code":
+            //            Console.WriteLine("Great choice!");
+            //            break;
+            //        case "Refactoring":
+            //            Console.WriteLine("Nice pick!");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Never heard of it");
+            //            break;
+            //    }
+            //}
+            #endregion
+
         }
     }
 }
